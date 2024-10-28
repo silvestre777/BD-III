a960e574acb9f89f3d225bf1d82694ae00c847f1
Silvestre Ferreira <168732596+silvestre777@users.noreply.github.com>
2024-10-28 14:24:12 +0000

    Atividade realizar crud

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # BD-III
+
+Criar um CRUD para um usuário:
+- Create (Criar usuario)
+- Read(listar todos usuário / pesquisar um usuário)
+- Update (Atualizar dados de um usuário)
+- Delete (excluir um usuário)
+
+No arquivo principal, crie um menu onde seja posivel escolher a ação desejada.
+
+        == SENAI SOLUTION ===
+1 - Adicionar Usuário
+2 - Pesquisar um usuário
+3 - Atualizar dados de um usuário
+4 - Excluir um usuário
+5 - Exibir todos os usuários cadastrados
+0 - Sair
+
+Informe a opção desejada: 
+...
+
+Os dados precisam ser salvos no banco de dados.
+
+### Tecnologias:
+- ORM: SQLALCHEMY
+- Banco de dados: MySQL
--- app/main.py
+++ app/main.py
@@ -7,17 +7,36 @@ def main():
     repository = UsuarioRepository(session)
     service = UsuarioService(repository)
 
+    def cadastrar_usuario():
+        print("\nAdicionando usuário.")
+        nome = input("Digite seu nome: ")
+        email = input("Digite seu e-mail: ")
+        senha = input("Digite sua senha: ")
+    #Funcao para armazenar os dados digitado a cima
+        service.criar_usuario(nome= nome, email = email, senha=senha)
+
 
     #Solicitando dados para o usuario.
+    print(f"\n== Tabela de serviços disponiveis ==")
+    print(f"1- Cadastrar Usuário")
+    print(f"2- Atualizar cadastro")
+    print(f"2- Consultar Usuário")
+    print(f"3- Excluir Usuário")
+    print(f"4- Listar Usuário")
+    opcao = input("Digite a opção desejada: ")
 
-    print("\nAdicionando usuário.")
-    nome = input("Digite seu nome: ")
-    email = input("Digite seu e-mail: ")
-    senha = input("Digite sua senha: ")
-    #Funcao para armazenar os dados digitado a cima
-    service.criar_usuario(nome= nome, email = email, senha=senha)
+    if opcao == 1:
+        cadastro = cadastrar_usuario()
+        return 
     
+
+    if opcao == 2:
+        print("dfwqdqw")
+
+
     #Listar todos os usuários cadastrados.
+
+
     print("\nListando usuários cadastrados.")
     listar_usuario = service.listar_todos_usuario()
     for usuario in listar_usuario:
